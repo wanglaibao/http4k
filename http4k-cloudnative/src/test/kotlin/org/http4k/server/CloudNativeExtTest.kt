c403a5bb2454e18d9891574f3ec0db0d3970c7c1
David Denton <denton.david@gmail.com>
2019-03-01 18:22:25 +0000

    fix break

--- http4k-cloudnative/src/test/kotlin/org/http4k/server/CloudNativeExtTest.kt
+++ http4k-cloudnative/src/test/kotlin/org/http4k/server/CloudNativeExtTest.kt
@@ -3,7 +3,7 @@ package org.http4k.server
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.http4k.cloudnative.env.Port
-import org.http4k.core.Request
+import org.http4k.core.HttpHandler
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.junit.Test
@@ -12,7 +12,7 @@ class CloudNativeExtTest {
 
     @Test
     fun `can HttpHandler to a server`() {
-        val handler = { r: Request -> Response(Status.OK) }
+        val handler = HttpHandler { Response(Status.OK) }
         assertThat(handler.asServer(::SunHttp, Port(8000)).port(), equalTo(8000))
     }
 }
