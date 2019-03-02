e58d28ac8eefd1a4279926a41f8afc683c103ac1
David Denton <denton.david@gmail.com>
2019-03-02 10:52:57 +0000

    remove unused request

--- http4k-cloudnative/src/test/kotlin/org/http4k/server/CloudNativeExtTest.kt
+++ http4k-cloudnative/src/test/kotlin/org/http4k/server/CloudNativeExtTest.kt
@@ -5,7 +5,6 @@ import com.natpryce.hamkrest.equalTo
 import kotlinx.coroutines.runBlocking
 import org.http4k.cloudnative.env.Port
 import org.http4k.core.HttpHandler
-import org.http4k.core.Request
 import org.http4k.core.Response
 import org.http4k.core.Status
 import org.junit.Test
@@ -14,7 +13,7 @@ class CloudNativeExtTest {
 
     @Test
     fun `can HttpHandler to a server`() = runBlocking {
-        val handler = HttpHandler { r: Request -> Response(Status.OK) }
+        val handler = HttpHandler { Response(Status.OK) }
         assertThat(handler.asServer(::SunHttp, Port(8000)).port(), equalTo(8000))
     }
 }
